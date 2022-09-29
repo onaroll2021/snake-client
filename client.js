7a01d4bc7485ba88cfd635b84a6cf1823c0daaf1
onaroll2021 <lukeli.onaroll@gmail.com>
2022-09-29 03:48:35 +0000

    complete the snake game

---
+++ client.js
@@ -0,0 +1,33 @@
+const net = require('net'); 
+const connect = function () {
+  const conn = net.createConnection({ 
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Connected, yeah!")
+  });
+  conn.on("connect", () => {
+    conn.write("Name: LLL");
+  });
+  conn.on("connect", () => {
+    conn.write("Move: up")
+  });
+  conn.on("data", () => {
+    console.log("testing if receiving messages");
+  });
+  conn.on("close", (ifError) => {
+    if(!ifError) {
+    console.log("you are doing nothing")
+    }
+  });
+  return conn; 
+};
+module.exports = {net, connect};
+
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
